5e6256fec0a74a00bee499a350cbfd05d58e74a3
Knute Axelson <kaxelson@gmail.com>
2023-10-02 09:38:28 -0500

    adding sink

---
+++ src/transforms/sink.js
@@ -0,0 +1,14 @@
+import {Transform} from 'node:stream'
+
+export const sink = () => {
+	return new Transform({
+		objectMode: true,
+		transform(chunk, encoding, callback) {
+			callback()
+		},
+		flush(callback) {
+			console.log('sink finished')
+			callback()
+		}
+	})
+}
---
+++ test/count.test.js
@@ -0,0 +1,8 @@
+import jest from 'jest'
+
+describe('count', () => {
+	test('count', () => {
+		// do something
+		expect(1).toBe(1)
+	})
+})
